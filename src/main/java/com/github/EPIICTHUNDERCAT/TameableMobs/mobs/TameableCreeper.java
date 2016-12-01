9d0a93621c8391b236ad816a0b94723846eb8e7d
EPIICTHUNDERCAT <rvillalobos102299@gmail.com>
2016-12-01 01:49:41 -0800

    Mini Commit

---
+++ src/main/java/com/github/EPIICTHUNDERCAT/TameableMobs/mobs/TameableCreeper.java
@@ -0,0 +1,32 @@
+package com.github.EPIICTHUNDERCAT.TameableMobs.mobs;
+
+import java.util.UUID;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.IEntityOwnable;
+import net.minecraft.entity.monster.EntityCreeper;
+import net.minecraft.world.World;
+
+public class TameableCreeper extends EntityCreeper implements IEntityOwnable{
+
+	
+	
+	public int j = 3;
+	public TameableCreeper(World worldIn) {
+		super(worldIn);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public UUID getOwnerId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Entity getOwner() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
